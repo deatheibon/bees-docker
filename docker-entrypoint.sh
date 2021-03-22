51a3aa0bdfc095c7e1b55b2861d13637174ab467
deatheibon <dg@digital-eastside.de>
2021-03-22 21:51:31 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-if [ ${HASH_TABLE} ]; then
+if [ ! -f ${HASH_TABLE} ]; then
 
 	mkdir /mnt/.beeshome
-	truncate -s ${HASH_TABLE_SIZE} "${HASH_TABLE}"
-	chmod 700 "${HASH_TABLE}"
+	truncate -s ${HASH_TABLE_SIZE} ${HASH_TABLE}
+	chmod 700 ${HASH_TABLE}
 fi
 bees --no-timestamps ${OPTIONS} /mnt
